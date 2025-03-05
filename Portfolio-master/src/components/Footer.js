f44633be9fbd1f900c36e871e81cc816e24cd1f6
Ajay Jawade <156791794+ajayjawade06@users.noreply.github.com>
2025-03-06 01:00:47 +0530

    Update Footer.js

--- Portfolio-master/src/components/Footer.js
+++ Portfolio-master/src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/ajujawade/"
+                href="https://www.linkedin.com/in/ajayjawade06/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
